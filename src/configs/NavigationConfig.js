db68eae6ec215be62ef202bc8120d4b0c27fe198
StokinDenis <karina-romanova-05@bk.ru>
2025-03-17 21:41:11 +0300

    setting up a new sidebar

--- src/configs/NavigationConfig.js
+++ src/configs/NavigationConfig.js
@@ -1,19 +1,175 @@
 import { 
-  DashboardOutlined
+  DashboardOutlined, 
+  AppstoreOutlined,
+  FileTextOutlined,
+  PieChartOutlined,
+  EnvironmentOutlined,
+  AntDesignOutlined,
+  SafetyOutlined,
+  StopOutlined,
+  DotChartOutlined,
+  MailOutlined,
+  MessageOutlined,
+  CalendarOutlined,
+  BulbOutlined,
+  InfoCircleOutlined,
+  CompassOutlined,
+  LayoutOutlined,
+  DesktopOutlined,
+  FileDoneOutlined,
+  CommentOutlined,
+  RobotOutlined,
+  PlusCircleOutlined,
+  FundOutlined,
+  ShoppingCartOutlined,
+  BookOutlined,
+  FileUnknownOutlined,
+  ProfileOutlined
 } from '@ant-design/icons';
-import { APP_PREFIX_PATH } from 'configs/AppConfig'
+import { APP_PREFIX_PATH, AUTH_PREFIX_PATH } from 'configs/AppConfig'
+
 
 const dashBoardNavTree = [{
-  key: 'home',
-  path: `${APP_PREFIX_PATH}/home`,
-  title: 'home',
+  key: 'dashboards',
+  path: `${APP_PREFIX_PATH}/dashboards`,
+  title: 'Основные',
   icon: DashboardOutlined,
   breadcrumb: false,
-  submenu: []
+  submenu: [
+    {
+      key: 'dashboards-default',
+      path: `${APP_PREFIX_PATH}/dashboards/default`,
+      title: 'Дашборд',
+      icon: DashboardOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'apps-ecommerce',
+      path: `${APP_PREFIX_PATH}/apps/ecommerce`,
+      title: 'Каталог',
+      icon: ShoppingCartOutlined,
+      breadcrumb: true,
+      submenu: [
+        {
+          key: 'apps-ecommerce-productList',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/product-list`,
+          title: 'Товары',
+          icon: '',
+          breadcrumb: true,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-addProduct',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/add-product`,
+          title: 'Категории',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-editProduct',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/edit-product/12`,
+          title: 'Коллекции',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-orders',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/orders`,
+          title: 'Комбо',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        }
+      ]
+    },
+    {
+      key: 'dashboards-analytic',
+      path: `${APP_PREFIX_PATH}/dashboards/analytic`,
+      title: 'sidenav.dashboard.analytic',
+      icon: DotChartOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'dashboards-sales',
+      path: `${APP_PREFIX_PATH}/dashboards/sales`,
+      title: 'sidenav.dashboard.sales',
+      icon: FundOutlined,
+      breadcrumb: false,
+      submenu: []
+    }
+  ]
 }]
 
+const appsNavTree = [{
+  key: 'apps',
+  path: `${APP_PREFIX_PATH}/apps`,
+  title: 'системные',
+  icon: AppstoreOutlined,
+  breadcrumb: false,
+  submenu: [
+    {
+      key: 'apps-mail',
+      path: `${APP_PREFIX_PATH}/apps/mail/inbox`,
+      title: 'sidenav.apps.mail',
+      icon: MailOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'apps-chat',
+      path: `${APP_PREFIX_PATH}/apps/chat`,
+      title: 'sidenav.apps.chat',
+      icon: MessageOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'apps-calendar',
+      path: `${APP_PREFIX_PATH}/apps/calendar`,
+      title: 'sidenav.apps.calendar',
+      icon: CalendarOutlined,
+      breadcrumb: true,
+      submenu: []
+    },
+    {
+      key: 'apps-project',
+      path: `${APP_PREFIX_PATH}/apps/project`,
+      title: 'sidenav.apps.project',
+      icon: BulbOutlined,
+      breadcrumb: true,
+      submenu: [
+        {
+          key: 'apps-project-list',
+          path: `${APP_PREFIX_PATH}/apps/project/list`,
+          title: 'sidenav.apps.project.list',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-project-scrumboard',
+          path: `${APP_PREFIX_PATH}/apps/project/scrumboard`,
+          title: 'sidenav.apps.project.scrumboard',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        }
+      ]
+    },
+   
+  ]
+}]
+
+
 const navigationConfig = [
-  ...dashBoardNavTree
+  ...dashBoardNavTree,
+  ...appsNavTree,
+
+
 ]
 
 export default navigationConfig;
